ea5ea6416a2cc099deb498855ddbbba9422d8cd3
repat <repat@repat.de>
2020-09-24 15:49:06 +1200

    Tables config

---
+++ config/tables.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Table Names
+    |--------------------------------------------------------------------------
+    */
+
+    'failed_jobs' => 'failed_jobs',
+    'password_resets' => 'password_resets',
+    'personal_access_tokens' => 'personal_access_tokens',
+    'user_favorites' => 'user_favorites',
+    'users' => 'users',
+];
